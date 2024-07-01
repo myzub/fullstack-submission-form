9a6300fb15d57505c4a3d4fc1ccc2e0ee742c703
myzub <zub.m345@gmail.com>
2024-07-01 12:42:36 +0200

    0.0.1 - initial commit

---
+++ src/main/java/com/fullstacksubmissionform/FullstackSubmissionFormApplication.java
@@ -0,0 +1,13 @@
+package com.fullstacksubmissionform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackSubmissionFormApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullstackSubmissionFormApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fullstacksubmissionform/FullstackSubmissionFormApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullstacksubmissionform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackSubmissionFormApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
